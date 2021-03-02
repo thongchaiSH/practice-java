804959451f300f7d7d03b524af00826a990795b9
thongchai <thongchai.sh@gmail.com>
2021-03-02 16:34:13 +0700

    update

--- spring-boot-elk/product/src/main/java/com/example/product/ProductController.java
+++ spring-boot-elk/product/src/main/java/com/example/product/ProductController.java
@@ -1,2 +1,15 @@
-package com.example.product;public class ProductController {
+package com.example.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping
+    public  void foo(){
+        
+    }
 }
