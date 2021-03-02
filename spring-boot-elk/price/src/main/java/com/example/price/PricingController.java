7590020ef94eddb7ef1fa5efea09b15aa0b33ab6
thongchai <thongchai.sh@gmail.com>
2021-03-02 16:46:13 +0700

    fix code missing

--- spring-boot-elk/price/src/main/java/com/example/price/PricingController.java
+++ spring-boot-elk/price/src/main/java/com/example/price/PricingController.java
@@ -1,2 +1,21 @@
-package com.example.price;public class PricingController {
+package com.example.price;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products/price")
+public class PricingController {
+    private static final Logger LOG = LoggerFactory.getLogger(PricingController.class);
+
+    @GetMapping
+    public  void foo(){
+        LOG.trace("This is a TRACE log");
+        LOG.debug("This is a DEBUG log");
+        LOG.info("This is an INFO log");
+        LOG.error("This is an ERROR log");
+    }
 }
--- spring-boot-elk/product/src/main/java/com/example/product/ProductController.java
+++ spring-boot-elk/product/src/main/java/com/example/product/ProductController.java
@@ -1,15 +1,22 @@
 package com.example.product;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping
     public  void foo(){
-        
+        LOG.trace("This is a TRACE log");
+        LOG.debug("This is a DEBUG log");
+        LOG.info("This is an INFO log");
+        LOG.error("This is an ERROR log");
     }
 }
